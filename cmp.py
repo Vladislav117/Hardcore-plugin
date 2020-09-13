c32e7bd47dba0fc23249b4cf8f5c8c844112f0cd
Vladislav117 <58992285+Vladislav117@users.noreply.github.com>
2020-09-13 22:20:44 +0300

    Delete cmp.py

--- cmp.py
+++
@@ -1,7 +0,0 @@
-f = open("build/libs/EP.jar", 'rb')
-newf = open("C:/Users/User/Desktop/серв/config/mods/EP.jar", "wb")
-newf.write(f.read())
-f.close()
-newf.close()
-# import os
-# os.startfile('C:/Users/User/Desktop/серв/start.bat')
